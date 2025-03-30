3b4cf4ecc9e407ef97160d94c2163407f4711761
Malena Rivera <malerivera48@gmail.com>
2025-03-30 12:42:38 -0300

    Se agrega verificacion de la fecha para el clima

--- Tp1/Avanzado/ServidorClimaHilo.java
+++ Tp1/Avanzado/ServidorClimaHilo.java
@@ -25,7 +25,6 @@ class ServidorClimaHilo extends Thread {
         this.clientSocket = socket;
         this.idSesion = id;
 
-
         try {
              // Inicializar los streams de comunicación con el cliente
             out = new PrintWriter(socket.getOutputStream(), true);
@@ -71,7 +70,7 @@ class ServidorClimaHilo extends Thread {
 
 
 
-/*Método que genera un clima aleatorio para Neuquén si la fecha proporcionada es válida.
+/*Método que genera un clima aleatorio si la fecha proporcionada es válida.
 
 Recibe como parametro una fecha con el formato 'dd/mm/yyyy'
 Devuelve un String con la condicion climatica (que se elige de manera aleatoria)
@@ -91,19 +90,58 @@ Devuelve un String con la condicion climatica (que se elige de manera aleatoria)
 
 
 
-/*Metodo para verificar si la fecha sigue el formato definido (dd/mm/aaaa)
+/*Metodo para verificar si la fecha sigue el formato definido (dd/mm/aaaa).
 
+Además, se verifica que la fecha sea una fecha real.
 Recibe como parametro una fecha y devuelve true si es valida o false en caso contrario
 */
     public static boolean esFechaValida(String fecha) {
         String[] partes = fecha.split("/");
-        boolean valida;
+        boolean valida=true;
 
         // La fecha debe estar separada en 3 partes y tener la estructura correcta
         if (partes.length != 3) //Si tiene menos de tres partes, no es valida
             valida = false;
-        else
-            valida = (partes[0].length() == 2 && partes[1].length() == 2 && partes[2].length() == 4); //verifica la cantidad de digitos para el dia, el mes y el año
+        else{
+
+            //Verificamos que la fecha sea real.
+            int dia = Integer.parseInt(partes[0]);
+            int mes= Integer.parseInt(partes[1]);
+            int anio= Integer.parseInt(partes[2]);
+
+            if(mes<1 || mes>12)
+                valida=false;
+            
+             // Para guardar la cantidad máxima de días para el mes ingresado
+              int diasEnMes=0;
+       
+            //Segun el mes que se ingresó, obtenemos la cantidad de dias de ese mes. 
+            switch (mes) {
+                
+              //Para el caso de enero, marzo, mayo, julio, agosto, octubre y diciembre
+              case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                diasEnMes = 31;
+                break;
+
+              //Para el caso de abril, junio, septiembre y noviembre
+              case 4: case 6: case 9: case 11:
+                diasEnMes = 30;
+                break;
+
+               //Para el caso de febrero 
+              case 2:
+                // Este mes es un caso especial dado que depende de si el año es bisiesto.
+                //Para que un año sea bisiesto tiene que ser divisible por 4, pero no por 100. Y tambien si es divisible por 400.
+
+                boolean esBisiesto = ((anio % 4 == 0 && anio % 100 != 0) || anio % 400 == 0);
+                diasEnMes = esBisiesto ? 29 : 28;  //Si es bisiesto, entonces seran 29 dias, caso contrario 28
+                break;
+            }
+
+            if(dia < 1 || dia > diasEnMes)
+                valida=false;
+        }
         return valida;
+
     }
 }
